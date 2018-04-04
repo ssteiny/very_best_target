7fd434ed376b75c4ee668cc4ee8dcf805499838c
Raghu Betina <raghu@firstdraft.com>
2018-04-04 05:35:49 +0000

    Add forms for new collection objects on show pages

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -6,6 +6,8 @@ class VenuesController < ApplicationController
   end
 
   def show
+    @dish = Dish.new
+    @bookmark = Bookmark.new
     @venue = Venue.find(params[:id])
 
     render("venues/show.html.erb")
